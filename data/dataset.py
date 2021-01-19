9091a29728a8ec128f2332ee1eb254275f0b8abf
krtbb <auri.tk2009@gmail.com>
2021-01-19 12:48:45 +0000

    init

--- data/dataset.py
+++ data/dataset.py
@@ -7,78 +7,46 @@ from torchvision import transforms as T
 import torchvision
 import cv2
 import sys
+import pickle
 
 
 class Dataset(data.Dataset):
 
-    def __init__(self, root, data_list_file, phase='train', input_shape=(1, 128, 128)):
-        self.phase = phase
-        self.input_shape = input_shape
+    def __init__(self, list_path, mode='train', insize=64):
+        self.list_path = list_path
+        self.target_dir = '/'.join(list_path.split('/')[:-1])
+        self.mode = mode
+        self.insize = insize
 
-        with open(os.path.join(data_list_file), 'r') as fd:
-            imgs = fd.readlines()
-
-        imgs = [os.path.join(root, img[:-1]) for img in imgs]
-        self.imgs = np.random.permutation(imgs)
-
-        # normalize = T.Normalize(mean=[0.5, 0.5, 0.5],
-        #                         std=[0.5, 0.5, 0.5])
-
-        normalize = T.Normalize(mean=[0.5], std=[0.5])
-
-        if self.phase == 'train':
+        with open(list_path) as f:
+            self.filenames = list(map(lambda x: x.strip(), f.readlines()))
+        with open(os.path.join(self.target_dir, 'i2l.pickle'), 'rb') as f:
+            self.i2l = pickle.load(f)
+        with open(os.path.join(self.target_dir, 'l2i.pickle'), 'rb') as f:
+            self.l2i = pickle.load(f)
+        
+        assert self.mode in ['train', 'test']
+        if self.mode == 'train':
             self.transforms = T.Compose([
-                T.RandomCrop(self.input_shape[1:]),
-                T.RandomHorizontalFlip(),
+                T.RandomRotation(30),
                 T.ToTensor(),
-                normalize
+                T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
             ])
-        else:
+        elif self.mode == 'test':
             self.transforms = T.Compose([
-                T.CenterCrop(self.input_shape[1:]),
                 T.ToTensor(),
-                normalize
+                T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
             ])
 
     def __getitem__(self, index):
-        sample = self.imgs[index]
-        splits = sample.split()
-        img_path = splits[0]
-        data = Image.open(img_path)
-        data = data.convert('L')
-        data = self.transforms(data)
-        label = np.int32(splits[1])
-        return data.float(), label
+        filepath = self.filenames[index]
+        image = Image.open(filepath)
+        image = self.transforms(image)
+        label = filepath.split('/')[-2]
+        return image.float(), self.l2i[label]
 
     def __len__(self):
-        return len(self.imgs)
+        return len(self.filenames)
 
-
-if __name__ == '__main__':
-    dataset = Dataset(root='/data/Datasets/fv/dataset_v1.1/dataset_mix_aligned_v1.1',
-                      data_list_file='/data/Datasets/fv/dataset_v1.1/mix_20w.txt',
-                      phase='test',
-                      input_shape=(1, 128, 128))
-
-    trainloader = data.DataLoader(dataset, batch_size=10)
-    for i, (data, label) in enumerate(trainloader):
-        # imgs, labels = data
-        # print imgs.numpy().shape
-        # print data.cpu().numpy()
-        # if i == 0:
-        img = torchvision.utils.make_grid(data).numpy()
-        # print img.shape
-        # print label.shape
-        # chw -> hwc
-        img = np.transpose(img, (1, 2, 0))
-        # img *= np.array([0.229, 0.224, 0.225])
-        # img += np.array([0.485, 0.456, 0.406])
-        img += np.array([1, 1, 1])
-        img *= 127.5
-        img = img.astype(np.uint8)
-        img = img[:, :, [2, 1, 0]]
-
-        cv2.imshow('img', img)
-        cv2.waitKey()
-        # break
-        # dst.decode_segmap(labels.numpy()[0], plot=True)
+    def get_classnum(self):
+        return len(self.i2l)
