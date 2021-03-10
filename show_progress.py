61d381c4820edbd5d0dd00286a03e2534655010f
krtbb <auri.tk2009@gmail.com>
2021-03-10 06:00:51 +0000

    added logscale option

--- show_progress.py
+++ show_progress.py
@@ -48,7 +48,8 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('target', type=str)
+    parser.add_argument('--ylog', action='store_true')
 
     args = parser.parse_args()
 
-    show_progress(args.target)
+    show_progress(args.target, ylog=args.ylog)
